bc0a1648eb14b5426b77b05095ff1f394be04760
Luke Eller <leller108@gmail.com>
2024-12-04 19:10:56 -0800

    add write to hack file

--- projects/6/assembler/Main.java
+++ projects/6/assembler/Main.java
@@ -1,6 +1,7 @@
 package assembler;
 
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
@@ -8,13 +9,16 @@ public class Main {
       String inputFile = args[0];
       Parser parser = new Parser(inputFile);
 
+      String outputFile = inputFile.split("\\.")[0] + ".hack";
+      FileWriter writer = new FileWriter(outputFile);
+
       while (parser.hasMoreCommands()) {
          parser.advance();
          if (parser.commandType() == CommandType.C_COMMAND) {
             String comp = Code.comp(parser.comp());
             String dest = Code.dest(parser.dest());
             String jump = Code.jump(parser.jump());
-            System.out.println("111" + comp + dest + jump);
+            writer.write(String.format("111%s%s%s\n", comp, dest, jump));
          } else {
             // A command
             int symbolInteger = Integer.parseInt(parser.symbol());
@@ -22,8 +26,9 @@ public class Main {
                "%16s", 
                Integer.toBinaryString(symbolInteger)).replace(" ", "0"
             );
-            System.out.println(symbolBinary);
+            writer.write(String.format("%s\n", symbolBinary));
          }
       }
+      writer.close();
    }
 }
