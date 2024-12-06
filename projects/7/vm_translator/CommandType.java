b57831a04b4d32d8e88a130bd958c871bf9663b9
Luke Eller <leller108@gmail.com>
2024-12-05 23:02:53 -0800

    add my thoughts for vm implementation

--- projects/7/vm_translator/CommandType.java
+++ projects/7/vm_translator/CommandType.java
@@ -2,6 +2,12 @@ package vm_translator;
 
 enum CommandType {
     C_ARITHMETIC,
+    C_PUSH,
+    C_POP,
     C_LABEL,
+    C_GOTO,
+    C_IF,
+    C_FUNCTION,
+    C_RETURN,
     C_CALL
 }
--- projects/7/vm_translator/Parser.java
+++ projects/7/vm_translator/Parser.java
@@ -1,5 +1,53 @@
 package vm_translator;
 
+// push constant 7 should become:
+//    write the value 7 to the stack (ie location pointed to by SP)
+//    increment SP (the value stored in R0)
+
+// push constant 8 should become:
+//    write the value 8 to the stack (ie location pointed to by SP)
+//    increment SP (the value stored in R0)
+
+// add should become:
+    // subtract 1 from the SP
+    // put the value at top of stack into D
+    // subtract 1 from the SP
+    // put D + top of stack value into top of stack
+    // increment SP
+
+/* 
+    push constant 7
+    @7
+    D=A 
+    @SP
+    A=M
+    M=D // write 7 to the top of stack
+    @SP
+    M=M+1 // increment the stack pointer
+
+    push constant 8
+    @8
+    D=A
+    @SP
+    A=M
+    M=D // write 8 to the top of the stack
+    @SP
+    M=M+1 // increment the stack pointer
+
+    add
+    @SP
+    M=M-1 // decrement stack pointer
+    A=M
+    D=M // put top of stack value into D
+    @SP
+    M=M-1 // decrement stack pointer
+    A=M
+    M=D+M // put D + top of stack value into top of stack
+    @SP
+    M=M+1 // increment stack pointer
+*/
+
+
 public class Parser {
 
     public Parser(String inputFile) {
