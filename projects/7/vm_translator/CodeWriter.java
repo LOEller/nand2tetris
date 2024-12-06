b6c3f6ac95ea187dafa9de5d0642d5509e677bb2
Luke Eller <leller108@gmail.com>
2024-12-06 13:05:47 -0800

    stage 1 VM translator

--- projects/7/vm_translator/CodeWriter.java
+++ projects/7/vm_translator/CodeWriter.java
@@ -1,8 +1,14 @@
 package vm_translator;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CodeWriter {
-    public CodeWriter(String outputFile) {
+    FileWriter writer;
+
+    public CodeWriter(String outputFile) throws IOException {
         // opens the outpit file and gets ready to write to it
+        writer = new FileWriter(outputFile);
     }
 
     public void setFileName(String fileName) {
@@ -10,17 +16,113 @@ public class CodeWriter {
         // new VM file is started
     }
 
-    public void writeArithmetic(String command) {
+    public void writeArithmetic(String command) throws IOException {
         // writes the assembly code that is the translation of
         // the given arithmetic command
+
+        // true is -1
+        // false is 0
+
+        if (command == "add") {
+            // adds the values on top of stack and puts result on top of stack
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("D=M\n");
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("M=D+M\n"); 
+            writer.write("@SP\n");
+            writer.write("M=M+1\n"); 
+        } else if (command == "sub") {
+            // subtracts the values on top of stack and puts result on top of stack
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("D=M\n");
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("M=D-M\n"); 
+            writer.write("@SP\n");
+            writer.write("M=M+1\n"); 
+        } else if (command == "and") {
+            // computes bitwise and of values on top of stack and put result on stack
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("D=M\n");
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("M=D&M\n"); 
+            writer.write("@SP\n");
+            writer.write("M=M+1\n");
+        } else if (command == "or") {
+            // computes bitwise or of values on top of stack and put result on stack
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("D=M\n");
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("M=D|M\n"); 
+            writer.write("@SP\n");
+            writer.write("M=M+1\n");
+        } else if (command == "not") {
+            // computes bitwise not of value on top of stack and puts result on stack
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("M=!M\n");
+            writer.write("@SP\n");
+            writer.write("M=M+1\n");
+        } else if (command == "neg") {
+            // computes arithmetic negation of value on top of stack and puts result on stack
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("M=-M\n");
+            writer.write("@SP\n");
+            writer.write("M=M+1\n");
+        } else if (command == "eq") {
+            // compares values on top of stack and puts true on top of stack
+            // if they are equal
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("D=M\n"); // put first val into D
+            writer.write("@SP\n");
+            writer.write("M=M-1\n"); 
+            writer.write("A=M\n");
+            writer.write("M=M-D\n"); // put diff into M
+            writer.write("M=!M\n"); // negate
+            // now the value in M is 0 if they are NOT equal
+            writer.write("@SP\n");
+            writer.write("M=M+1\n");
+        }
     }
 
-    public void writePushPop(CommandType command, String segment, int index) {
+    public void writePushPop(CommandType command, String segment, int index) throws IOException {
         // writes the assembly code that is the translation of the 
         // given command, where command is either C_PUSH or C_POP
+
+        if (command == CommandType.C_PUSH) {
+            // pushes a constant index on to the stack
+            writer.write(String.format("@%d\n", index));
+            writer.write("D=A\n");
+            writer.write("@SP\n");
+            writer.write("A=M\n");
+            writer.write("M=D\n");
+            writer.write("@SP\n");
+            writer.write("M=M+1\n");
+        }
     }
 
-    public void close() {
+    public void close() throws IOException {
         // closes the output file
+        writer.close();
     }
 }
--- projects/7/vm_translator/Parser.java
+++ projects/7/vm_translator/Parser.java
@@ -1,5 +1,12 @@
 package vm_translator;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 // push constant 7 should become:
 //    write the value 7 to the stack (ie location pointed to by SP)
 //    increment SP (the value stored in R0)
@@ -15,70 +22,94 @@ package vm_translator;
     // put D + top of stack value into top of stack
     // increment SP
 
-/* 
-    push constant 7
-    @7
-    D=A 
-    @SP
-    A=M
-    M=D // write 7 to the top of stack
-    @SP
-    M=M+1 // increment the stack pointer
-
-    push constant 8
-    @8
-    D=A
-    @SP
-    A=M
-    M=D // write 8 to the top of the stack
-    @SP
-    M=M+1 // increment the stack pointer
-
-    add
-    @SP
-    M=M-1 // decrement stack pointer
-    A=M
-    D=M // put top of stack value into D
-    @SP
-    M=M-1 // decrement stack pointer
-    A=M
-    M=D+M // put D + top of stack value into top of stack
-    @SP
-    M=M+1 // increment stack pointer
-*/
-
 
 public class Parser {
+    String[] commands;
+    int commandIndex = -1;
 
-    public Parser(String inputFile) {
-        // opens the input file and gets ready to parse it
+    public Parser(String fileName) throws FileNotFoundException, IOException {
+        // opens the file stream and gets ready to parse it
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        commands = readFile(bufferedReader);
+        bufferedReader.close();
+    }
+
+    private String[] readFile(BufferedReader reader) throws IOException{
+        // reads all lines from a buffered reader to an array
+        // ignores comments, strips white space, and ignores blank lines
+        List<String> lines = new ArrayList<String>();
+        String line = null;
+
+        while ((line = reader.readLine()) != null) {
+            if (!line.isEmpty() && !line.startsWith("//")) {
+                lines.add(line);
+            }
+        }
+
+        // convert array list of lines to an array
+        return lines.toArray(new String[lines.size()]);
     }
 
     public boolean hasMoreCommands() {
-        // are there more commands in the input?
-        return true;
+        return commandIndex < commands.length - 1;
     }
 
-    public void advance() {
-        // makes the next command the current command
-        // should only be called is hasMoreCommands is true
+    public void advance() throws IOException {
+        // reads the next command from the input and makes it the current
+        // command. Should only be called if hasMoreCommands is true.
+        // Initially there is no current command
+        commandIndex++;
+    }
+
+    private String[] commandComponents() {
+        return commands[commandIndex].split("\\s");
     }
 
     public CommandType commandType() {
         // returns the type of the current VM command
-        return CommandType.C_ARITHMETIC;
+        switch(commandComponents()[0]) {
+            case "push":
+                return CommandType.C_PUSH;
+            case "pop":
+                return CommandType.C_POP;
+            case "add":
+                return CommandType.C_ARITHMETIC;
+            case "sub":
+                return CommandType.C_ARITHMETIC;
+            case "neq":
+                return CommandType.C_ARITHMETIC;
+            case "eq":
+                return CommandType.C_ARITHMETIC;
+            case "gt":
+                return CommandType.C_ARITHMETIC;
+            case "lt":
+                return CommandType.C_ARITHMETIC;
+            case "and":
+                return CommandType.C_ARITHMETIC;
+            case "or":
+                return CommandType.C_ARITHMETIC;
+            case "not":
+                return CommandType.C_ARITHMETIC;
+            default:
+                return null;
+        }
+    }
+
+    public String command() {
+        // returns the current command
+        return commandComponents()[0];
     }
 
     public String arg1() {
         // returns the first argument of the current command
         // should not be called if the current command is C_RETURN
-        return "";
+        return commandComponents()[1];
     }
 
     public int arg2() {
         // returns the second argument of the current command
         // should be called only if the current command is C_PUSH,
         // C_POP, C_FUNCTION, or C_CALL
-        return 0;
+        return Integer.parseInt(commandComponents()[2]);
     }
 }
